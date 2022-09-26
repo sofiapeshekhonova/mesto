41c54d9833674f1421725daea32797126be7a525
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2022-09-26 21:35:52 +0300

    пр 6

--- scripts/index.js
+++ scripts/index.js
@@ -118,3 +118,108 @@ closeImage.addEventListener('click', evt => {
 closenewPlace.addEventListener('click', evt => {
   closePopup(popupIntro);
 });
+
+
+// Проектная работа 6
+
+// Функция, которая добавляет класс с ошибкой + текст ошибки 2 параметром
+const showInputError = (formProfile, textInput, errorMessage) => {
+  // Находим элемент ошибки внутри самой функции
+  const formError = formProfile.querySelector(`.${textInput.id}-error`); 
+
+  textInput.classList.add('form__text_type_error');
+// Заменим содержимое span с ошибкой на переданный параметр
+  formError.textContent = errorMessage;
+   // Показываем сообщение об ошибке
+  formError.classList.add('form__text-error_active');
+};
+
+// Функция, которая удаляет класс с ошибкой
+const hideInputError = (formProfile, textInput) => {
+  const formError = formProfile.querySelector(`.${textInput.id}-error`); 
+  textInput.classList.remove('form__text_type_error');
+  formError.classList.remove('form__text-error_active');
+// Очистим ошибку
+  formError.textContent = '';
+};
+
+// Функция, которая проверяет валидность поля
+const isValid = (formProfile, textInput) => {
+  if (!textInput.validity.valid) {
+    // showInputError теперь получает параметром форму, в которой находится проверяемое поле, и само это поле
+    // Если поле не проходит валидацию, покажем ошибку + 2- сообщение об ошибке
+    showInputError(formProfile, textInput, textInput.validationMessage);
+  } else {
+    // hideInputError теперь получает параметром форму, в которой находится проверяемое поле, и само это поле
+    // Если проходит, скроем
+    hideInputError(formProfile, textInput);
+  }
+};
+
+const setEventListeners = (formProfile) => {
+  // Находим все поля внутри формы, сделаем из них массив методом Array.from
+  const inputList = Array.from(formProfile.querySelectorAll('.form__text'));
+    // Найдём в текущей форме кнопку отправки
+    const buttonElement = formProfile.querySelector('.form__save');
+    //нужно ли чтобы была заблокированна в самом начале?
+    // changeButton(inputList, buttonElement);
+  // Обойдём все элементы полученной коллекции
+  inputList.forEach((textInput) => {
+    // каждому полю добавим обработчик события input
+    textInput.addEventListener('input', function () {
+      // Внутри колбэка вызовем isValid передав ей форму и проверяемый элемент
+      isValid(formProfile, textInput)
+      // Вызовем toggleButtonState и передадим ей массив полей и кнопку
+      changeButton(inputList, buttonElement);
+    });
+  });
+}; 
+
+const enableValidation = () => {
+  // Найдём все формы с указанным классом в DOM,
+  // сделаем из них массив методом Array.from
+  const formList = Array.from(document.querySelectorAll('.form'));
+  // Переберём полученную коллекцию
+  formList.forEach((formProfile) => {
+    formProfile.addEventListener('submit', function (evt) {
+            evt.preventDefault();
+    });
+    // если бы внутри формы были другие формы (несколько шагов заполнения -шаг - филсет)
+    // const fieldsetList = Array.from(formProfile.querySelectorAll('.form__set'));
+    // fieldsetList.forEach((fieldset) => {
+    //   setEventListeners(fieldset);
+    // })
+
+    // Для каждой формы вызовем функцию setEventListeners, передав ей элемент формы
+    setEventListeners(formProfile);
+   
+  });
+};
+
+// Вызовем функцию
+enableValidation(); 
+
+// Функция принимает массив полей, проверяет наличие невалидного поля и сигнализирует, можно ли разблокировать кнопку сабмита
+const hasfalseInput = (inputList) => {
+  // проходим по этому массиву методом some
+  return inputList.some((textInput) => {
+// Если поле не валидно, колбэк вернёт true
+// Обход массива прекратится и вся функция hasInvalidInput вернёт true
+    return !textInput.validity.valid;
+  })
+}; 
+
+// Функция принимает массив полей ввода и элемент кнопки, состояние которой нужно менять
+const changeButton = (inputList, buttonElement) => {
+  // Если есть хотя бы один невалидный инпут
+  if (hasfalseInput(inputList)) {
+    // сделай кнопку неактивной
+    buttonElement.classList.add('form__save_inactive');
+    buttonElement.setAttribute('disabled', true);
+  } else {
+    // иначе сделай кнопку активной
+    buttonElement.classList.remove('form__save_inactive');
+    buttonElement.removeAttribute('disabled', true);
+  }
+}; 
+

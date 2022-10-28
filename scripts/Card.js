9394661ac6784adf99ccd502f574449b653e2a14
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2022-10-28 13:13:05 +0300

    refacor: исправила ошибки после 1 проверки

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,86 +1,54 @@
-export const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 export default class Card {
-  constructor(data, templateSelector) {
-    this._link = data.link
+  constructor(data, templateSelector, handleOpenPopup) {
+    this._link = data.link;
     this._name = data.name;
     this._templateSelector = templateSelector;
+    this._handleOpenPopup = handleOpenPopup
   }
 
-  _getTemplate() {  // забираем разметку из HTML и клонируем элемент
-  const cardElement = document
-  .querySelector(this._templateSelector)
-  .content
-  .querySelector('.photo-card')
-  .cloneNode(true);
-
-// вернём DOM-элемент карточки
-  return cardElement;
+  _getTemplate() {
+    // забираем разметку из HTML и клонируем элемент
+    const cardElement = document
+      .querySelector(this._templateSelector)
+      .content.querySelector(".photo-card")
+      .cloneNode(true);
+    // вернём DOM-элемент карточки
+    return cardElement;
   }
 
   generateCard() {
     // Запишем разметку в приватное поле _element. Так у других элементов появится доступ к ней.
     this._element = this._getTemplate();
-    this._setEventListeners();
 
     // Добавим данные
-    this._element.querySelector('.photo-card__picture').src = this._link;
-    this._element.querySelector('.photo-card__description-text').textContent = this._name;
-    this._element.querySelector('.photo-card__picture').alt = this._name;
+    this._element.querySelector(".photo-card__picture").src = this._link;
+    this._element.querySelector(".photo-card__description-text").textContent = this._name;
+    this._element.querySelector(".photo-card__picture").alt = this._name;
+
+    this._likeButton =  this._element.querySelector(".photo-card__description-like");
+    this._wasteBasketButton= this._element.querySelector(".photo-card__wastebasket");
+    this._setEventListeners();
 
     return this._element;
   }
 
-   _handleCardLike() {
-    this._element.querySelector('.photo-card__description-like').classList.toggle('photo-card__description-like_active');
-  }
-
-  _handleOpenPopup() {
-    picture.src = this._link;
-    pictureCaption.textContent = this._name;
-    openPopup(popupPicture);
+  _handleCardLike() {
+    this._likeButton.classList.toggle("photo-card__description-like_active");
   }
 
   _handleDeleteCard() {
-      this._element.remove();
+    this._element.remove();
   }
 
   _setEventListeners() {
-    this._element.querySelector('.photo-card__description-like').addEventListener('click', () => {
-      this._handleCardLike();
-    });
-    this._element.querySelector('.photo-card__wastebasket').addEventListener('click', () => {
-      this._handleDeleteCard();
-    });
-    this._element.querySelector('.photo-card__picture').addEventListener('click', () => {
-      this._handleOpenPopup();
-    });
+    this._likeButton.addEventListener("click", () => {
+        this._handleCardLike();
+      });
+      this._wasteBasketButton.addEventListener("click", () => {
+        this._handleDeleteCard();
+      });
+    this._element.querySelector(".photo-card__picture").addEventListener("click", () => {
+        this._handleOpenPopup(this._name, this._link);
+      });
   }
 }
-
-
--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -7,81 +7,92 @@ export default class FormValidator {
     this._inputErrorClass = data.inputErrorClass;
     this._errorClass = data.errorClass;
     this._formElement = formElement;
-    this._inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector));
-  // Найдём все формы с указанным классом в DOM, сделаем из них массив методом Array.from
-   this._buttonElement = this._formElement.querySelector(this._submitButtonSelector);
+    // Найдём все формы с указанным классом в DOM, сделаем из них массив методом Array.from
+    this._inputList = Array.from(
+      this._formElement.querySelectorAll(this._inputSelector)
+    );
+    //из нужной формы берем кнопку сохранить
+    this._buttonElement = this._formElement.querySelector(
+      this._submitButtonSelector //кнопка сохранить
+    );
   }
 
-    enableValidation() {
-      this._setEventListeners();
-    }
+  enableValidation() {
+    this._setEventListeners();
+  }
 
-    _isValid(textInput) {
-      if (!textInput.validity.valid) {
-        // Если поле не проходит валидацию, покажем ошибку + 2- сообщение об ошибке
-        this._showInputError(textInput, textInput.validationMessage);
-      } else {
-        // Если проходит, скроем
-        this._hideInputError(textInput);
-      }
+  __toggleInputErrorState(textInput) {
+    if (!textInput.validity.valid) {
+      // Если поле не проходит валидацию, покажем ошибку + 2- сообщение об ошибке
+      this._showInputError(textInput, textInput.validationMessage);
+    } else {
+      // Если проходит, скроем
+      this._hideInputError(textInput);
     }
-      _setEventListeners() {
-      // чтобы была заблокированна в самом начале
-      this._changeButton();
-      // Обойдём все элементы полученной коллекции
-        this._inputList.forEach((textInput) => {
-     // каждому полю добавим обработчик события input
-          textInput.addEventListener('input', () => {
-      // Внутри колбэка вызовем isValid передав ей проверяемый элемент
-            this._isValid(textInput)
-            //заблок кнопка
-           this._changeButton();
-          });
+  }
+
+  _setEventListeners() {
+    // чтобы была заблокированна в самом начале
+    this._changeShapeButton();
+    // Обойдём все элементы полученной коллекции
+    this._inputList.forEach((textInput) => {
+      // каждому полю добавим обработчик события input
+      textInput.addEventListener("input", () => {
+        // Внутри колбэка вызовем _toggleInputErrorState передав ей проверяемый элемент
+        this.__toggleInputErrorState(textInput);
+        //заблок кнопка
+        this._changeShapeButton();
       });
-    }
+    });
+  }
 
-    _changeButton() {
-      if (this._hasfalseInput(this._inputList)) {
-            // сделай кнопку неактивной
-            this.makeDisableButton(this._buttonElement);
-          } else {
-            // иначе сделай кнопку активной
-            this.removeDisableButton(this._buttonElement)
-          }
+  _changeShapeButton() {
+    if (this._hasFalseInput(this._inputList)) {
+      // сделай кнопку неактивной
+      this._makeDisableButton(this._buttonElement);
+    } else {
+      // иначе сделай кнопку активной
+      this._removeDisableButton(this._buttonElement);
     }
+  }
 
-    _hasfalseInput() {
-      // проходим по этому массиву методом some
-      return this._inputList.some((textInput) => {
-    // Если поле не валидно, колбэк вернёт true
-    // Обход массива прекратится и вся функция hasInvalidInput вернёт true
-        return !textInput.validity.valid;
-      })
-    };
+  _hasFalseInput() {
+    // проходим по этому массиву методом some
+    return this._inputList.some((textInput) => {
+      // Если поле не валидно, колбэк вернёт true
+      // Обход массива прекратится и вся функция hasInvalidInput вернёт true
+      return !textInput.validity.valid;
+    });
+  }
 
-    makeDisableButton(element) {
-      element.classList.add(this._inactiveButtonClass);
-      element.setAttribute('disabled', true);
-    }
+  _makeDisableButton() {
+    this._buttonElement.classList.add(this._inactiveButtonClass); //добавляем класс неактивной копки
+    this._buttonElement.setAttribute("disabled", true); //делаем кнопку неактивной
+  }
 
-    removeDisableButton(element) {
-      element.classList.remove(this._inactiveButtonClass);
-      element.removeAttribute('disabled');
-    }
+  _removeDisableButton() {
+    this._buttonElement.classList.remove(this._inactiveButtonClass);  //убираем
+    this._buttonElement.removeAttribute("disabled"); // убираем
+  }
 
   _showInputError(textInput, errorMessage) {
-      // Находим элемент ошибки внутри самой функции
+    // Находим элемент ошибки внутри самой функции
     this._formError = this._formElement.querySelector(`.${textInput.id}-error`);
     textInput.classList.add(this._inputErrorClass);
     // Показываем сообщение об ошибке
     this._formError.classList.add(this._errorClass);
     this._formError.textContent = errorMessage;
   }
-  _hideInputError(textInput)  {
+
+  _hideInputError(textInput) {
     this._formError = this._formElement.querySelector(`.${textInput.id}-error`);
     textInput.classList.remove(this._inputErrorClass);
     this._formError.classList.remove(this._errorClass);
-  // Очистим ошибку
-    this._formError.textContent = '';
-  };
+    // Очистим ошибку
+    this._formError.textContent = "";
+  }
+
+  disableErrorMessages() {
+    this._inputList.forEach(e => this._hideInputError(e));
+  }
 }
